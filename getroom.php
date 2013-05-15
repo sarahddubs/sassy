369a68b3d49331fd9cbed91ed220c7fe7c5aa67f
Sarah <sarahddubs@gmail.com>
2013-05-14 23:05:23 -0700

    Secured convos folder

--- getroom.php
+++ getroom.php
@@ -1,4 +1,12 @@
 <?php
+	switch ($_SERVER['HTTP_ORIGIN']) {
+	    case 'http://campusconvoz.com': case 'https://campusconvoz.com':
+	    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+	    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+	    header('Access-Control-Max-Age: 1000');
+	    header('Access-Control-Allow-Headers: Content-Type');
+	    break;
+	}
 	$my_file = 'rooms.txt';
 	$handle = fopen($my_file, 'r');
 	$data = fread($handle,100);
--- writeroom.php
+++ writeroom.php
@@ -1,4 +1,13 @@
 <?php
+	switch ($_SERVER['HTTP_ORIGIN']) {
+	    case 'http://campusconvoz.com': case 'campusconvoz.com':
+	    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+	    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+	    header('Access-Control-Max-Age: 1000');
+	    header('Access-Control-Allow-Headers: Content-Type');
+	    break;
+	}
+	
 	$my_file = 'rooms.txt';
 	$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
 	$data = $_POST['chatroom_id'];
