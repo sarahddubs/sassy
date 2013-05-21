9f16cb98127bd97bdd978099569a5c9e8076ade0
Scott Khamphoune <scottk92@stanford.edu>
2013-05-20 19:55:53 -0700

    added google analytics

--- chat.php
+++ chat.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="loader.css" type="text/css" />
 </head>
 <body>
+	<?php include_once("analyticstracking.php") ?>
 	<div id="page-wrap">
 		<h2>Campus Convoz</h2>
         <p id="name-area"></p>
@@ -184,6 +185,17 @@
 	}
   
   
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41092222-1', 'campusconvoz.com');
+  ga('send', 'pageview');
+
 </script>
 
 </body>
